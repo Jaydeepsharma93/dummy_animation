5b91fb5c843b10112b96b7da4773a5b921ab6b05
jaydeepsharma93 <sharmajaydeep1515@gmail.com>
2024-07-16 03:43:16 +0530

    Initial commit

---
+++ lib/src/onboarding/widgets/onboarding_screen_widgets.dart
@@ -0,0 +1,4 @@
+export 'package:dummy_animation/src/onboarding/widgets/animated_button_widget.dart';
+export 'package:dummy_animation/src/onboarding/widgets/animated_description_widget.dart';
+export 'package:dummy_animation/src/onboarding/widgets/animated_dish_widget.dart';
+export 'package:dummy_animation/src/onboarding/widgets/animated_title_widget.dart';
